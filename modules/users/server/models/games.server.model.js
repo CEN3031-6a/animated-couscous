a4c25583e470570855bcdeb1ee1fbb51ec4c1214
Nick <ncritelli1@ufl.edu>
2016-02-17 17:02:11 -0500

    created games schema

---
+++ modules/users/server/models/games.server.model.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  crypto = require('crypto'),
+  validator = require('validator'),
+  generatePassword = require('generate-password'),
+  owasp = require('owasp-password-strength-test');
+
+/**
+ * Games Schema
+ */
+var GamesSchema = new Schema({
+  Title: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  Platform: {
+    type: String,
+    default: ''
+  },
+  salt: {
+    type: String
+  },
+  gameImageURL: {
+    type: String,
+    default: 'modules/users/client/img/profile/default.png'
+  },
+  updated: {
+    type: Date
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  discussions: [],
+});
+
+
+GamesSchema.pre('save', function (next) {
+
+var currentDate = new Date();
+this.updated = currentDate;
+if (!this.created)
+	this.created = currentDate;
+
+  next();
+});
+
+
+mongoose.model('Games', GamesSchema);
