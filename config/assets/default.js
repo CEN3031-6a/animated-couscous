f30ff26eb6159390feb17929615214e2dae7a514
Nick <nimamshah@ufl.edu>
2016-03-14 20:47:58 -0400

    Implement Carousel functionality and add related dependencies

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,9 +5,12 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/slick-carousel/slick/slick.css',
+        'public/lib/slick-carousel/slick/slick-theme.css'
       ],
       js: [
+        'public/lib/jquery/dist/jquery.js',
         'public/lib/angular/angular.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
@@ -16,13 +19,17 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/slick-carousel/slick/slick.js',
+        'public/lib/angular-slick/dist/slick.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
     css: [
       'modules/*/client/css/*.css', 
-      'modules/core/client/css/*.css' 
+      'modules/core/client/css/*.css',
+      'public/lib/slick-carousel/slick/slick.css',
+      'public/lib/slick-carousel/slick/slick-theme.css'   
       //'modules/users/css/*.css'
     ],
     less: [
