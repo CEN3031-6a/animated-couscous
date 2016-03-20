bb661def55de94d130cb2eb62bf11bb31be7f56a
Nick <nimamshah@ufl.edu>
2016-03-20 15:23:49 -0400

    Initial commit for list-games branch

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,15 +4,5 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-
-    $scope.find = function() {
-    	$scope.loading = true;
-
-    	
-    };
-
-    $scope.findOne = function() {
-
-    };
-	}
+  }
 ]);
