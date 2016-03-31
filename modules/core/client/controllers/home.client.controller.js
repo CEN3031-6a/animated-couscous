def96e293676acb4ab3d03fbdaf1cb6d08001cd7
Nick <nimamshah@ufl.edu>
2016-03-31 15:57:23 -0400

    Work on search for games, rendering messes up after searching/clearing

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    
+    $scope.query = "";
+    $scope.userInput = "";
+    $scope.applySearch = function() {
+      $scope.query = $scope.userInput;
+    };
   }
 ]);
